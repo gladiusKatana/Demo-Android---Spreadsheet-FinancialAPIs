b67341e53919dd4ad7d80072eeee7d45b42b7941
gladiusKatana <garthsnyder@protonmail.com>
2023-10-17 01:22:00 -0400

    basic v0 based off iOS app in Swift

--- app/src/main/java/com/example/stack/MainActivity.kt
+++ app/src/main/java/com/example/stack/MainActivity.kt
@@ -1,43 +1,76 @@
-package com.example.stack
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.stack.ui.theme.StackTheme
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            StackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+class Node(
+    val order: Int,
+    var value: Double,
+    var dependency: Dependency? = null
+) {
+    var valueChanged: (Double) -> Unit = {}
+
+    init {
+        dependency?.nodes?.forEach { node ->
+            node.valueChanged = { updateValue() }
+        }
+        updateValue()
+    }
+
+    private fun updateValue() {
+        dependency?.let {
+            value = it.computation(it.nodes.map { node -> node.value })
+            valueChanged(value)
+        }
+    }
+}
+
+data class Dependency(
+    val nodes: List<Node>,
+    val computation: (List<Double>) -> Double
+)
+
+// NetworkManager, APIFetchingGridViewModel, DashboardViewModel, and other networking classes will be similar to Swift's implementation,
+// just translated into Kotlin and adapted for Android networking libraries such as Retrofit or OkHttp.
+
+@Composable
+fun GridView(viewModel: GridViewModel) {
+    val nodes by viewModel.nodes.collectAsState()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        nodes.forEach { node ->
+            Text(text = node.value.toString())
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+@Preview
+fun PreviewGridView() {
+    // Provide a mocked ViewModel or a PreviewViewModel to show a preview of the GridView.
+    // GridView(PreviewViewModel())
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StackTheme {
-        Greeting("Android")
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column {
+                // Your Composables here, e.g., GridView
+            }
+        }
     }
 }
+
+// Remember to handle Android lifecycle events, network permissions, and error handling, which might be different or additional compared to iOS/Swift.
