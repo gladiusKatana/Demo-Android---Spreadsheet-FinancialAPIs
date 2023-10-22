c1c3c424a6c5cc044cc08f5dfefeb35497c4f18d
gladiusKatana <garthsnyder@protonmail.com>
2023-10-21 23:20:36 -0400

    refactor, remove logging

--- app/src/main/java/com/example/stack/MainActivity.kt
+++ app/src/main/java/com/example/stack/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.example.stack
-
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.ViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,9 +95,6 @@ fun GridView(viewModel: GridViewModel, modifier: Modifier = Modifier) {
                             .clickable(enabled = !isDependent) {
                                 if (!isDependent) {
                                     viewModel.incrementNodeValue(node)
-                                    Log.d("GridView", "Cell clicked, value = ${node.value}")
-                                } else {
-                                    Log.d("GridView", "Dependent cell clicked, no action taken.")
                                 }
                             },
                         contentAlignment = Alignment.Center
