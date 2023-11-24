24bb9935a36f648178a102686546898410320422
gladiusKatana <garthsnyder@protonmail.com>
2023-11-23 19:06:24 -0500

    colors: add dark mode compatibility (status bar)

--- app/src/main/java/com/example/stack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stack/ui/theme/Theme.kt
@@ -1,6 +1,9 @@
 package com.example.stack.ui.theme
 
 import android.app.Activity
+import android.os.Build
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
@@ -10,17 +13,16 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 val black = Color.Black
 val white = Color.White
 
 val DarkColors = darkColors(
-    primary = white,
+    primary = Color.DarkGray,               // should be same as 'surface' below
     primaryVariant = Color.LightGray,
     secondary = white,
     background = black,
-    surface = Color.DarkGray,
+    surface = Color.DarkGray,               //<-- alternative: Color.Black, (maybe make this a user setting)
     onPrimary = black,
     onSecondary = black,
     onBackground = white,
@@ -28,11 +30,11 @@ val DarkColors = darkColors(
 )
 
 val LightColors = lightColors(
-    primary = black,
-    primaryVariant = Color.DarkGray,
+    primary = white,                        // should be same as 'surface' below
+    primaryVariant = Color.LightGray,
     secondary = black,
     background = white,
-    surface = white,                        //<-- alternative: Color.LightGray,
+    surface = white,                        //<-- alternative: Color.LightGray, (maybe make this a user setting)
     onPrimary = white,
     onSecondary = white,
     onBackground = black,
@@ -46,17 +48,43 @@ fun StackTheme(
 ) {
     val colors = if (darkTheme) DarkColors else LightColors
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colors.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+
+            window.statusBarColor = colors.primary.toArgb() // Sets the status bar color.
+
+            // Check for Build Version and use WindowInsetsController if available
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val windowInsetsController = window.insetsController
+                if (darkTheme) {
+                    windowInsetsController?.setSystemBarsAppearance(
+                        0, // clear the appearance flag for light status bars
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                    )
+                } else {
+                    windowInsetsController?.setSystemBarsAppearance(
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                    )
+                }
+            } else {
+                // For older versions, set the status bar color and light status bar appearance
+                @Suppress("DEPRECATION")
+                if (darkTheme) {
+                    window.decorView.systemUiVisibility = 0 // Clear flags
+                } else {
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
+            }
         }
     }
 
     MaterialTheme(
         colors = colors,
         typography = Typography,
+        //shapes = Shapes,
         content = content
     )
 }
